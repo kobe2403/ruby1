5b7135e54c530130bd0bbdbe7ce64294681daa68
kobe2403 <kobe2403.momo@gmail.com>
2019-11-19 00:16:30 +0900

    bootstrapを導入した

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get "", to: "posts#index"#post=これから作成するcontrollerの名前、indexはcontrollerに設定する関数名	
 end
